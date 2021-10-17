175f0a877b57e708d3f8b690ae2ff30f2cfc1ab9
Edelgard <jtboyman@gmail.com>
2021-10-16 20:42:13 -0400

    reusable without refreshing page, resets original array'
    git status
    
    '
    
    '
    
    '
                                        peporepor
    ''\''g'\''
    
    'abc'\''abc'
    abc'abc
    $ echo 'abc'\''abc'
    abc'abc
    $ echo abc"abc
    abcabc

--- password-generator/assets/js/script.js
+++ password-generator/assets/js/script.js
@@ -1,8 +1,6 @@
 // Assignment code here
 
-//function generatePassword() {}
-
-var userPasswordMaker = [];
+let userPasswordMaker = [];
 const lowercaseOptions = ["a","b","c","d","e","f","g","h",'i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 const uppercaseOptions = ["A","B","C","D","E","F","G","H",'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const numericOptions = ["0",'1','2','3','4','5','6','7','8','9'];
@@ -143,7 +141,13 @@ let randomCharacter = function() {
   return value;
 };
 
+//reset
+let getReady = function() {
+  userPasswordMaker = [];
+}
+
 let generatePassword = function() {
+  getReady();
   getLength();
   confirmLowercase();
   confirmUppercase();
